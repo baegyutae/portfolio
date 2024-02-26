7b195803442b6c6ec7862ec15526562ccdb0576a
gyutae <qoxowkd0716@gmail.com>
2024-02-26 19:50:55 +0900

    feat: Post 빌더 추가

--- src/main/java/baegyutae/portfolio/domain/model/Post.java
+++ src/main/java/baegyutae/portfolio/domain/model/Post.java
@@ -6,10 +6,14 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Post {
 
     @Id
@@ -23,4 +27,12 @@ public class Post {
 
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
+
+    @Builder
+    public Post(String title, String content) {
+        this.title = title;
+        this.content = content;
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
 }
