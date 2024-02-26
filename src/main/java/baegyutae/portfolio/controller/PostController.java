3ef1c2d2fa6d30f8744762b94f9f80b88d74de96
gyutae <qoxowkd0716@gmail.com>
2024-02-26 20:15:06 +0900

    Feat: deletePost 구현

--- src/main/java/baegyutae/portfolio/controller/PostController.java
+++ src/main/java/baegyutae/portfolio/controller/PostController.java
@@ -7,6 +7,7 @@ import baegyutae.portfolio.service.impl.PostServiceImpl;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -43,5 +44,11 @@ public class PostController {
     @ResponseStatus(HttpStatus.OK)
     public void updatePost(@PathVariable Long id, @RequestBody PostUpdateDto postUpdateDto) {
         postServiceImpl.updatePost(id, postUpdateDto);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deletePost(@PathVariable Long id) {
+        postServiceImpl.deletePost(id);
     }
 }
