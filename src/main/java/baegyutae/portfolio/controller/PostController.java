bc346b6b47c9bafa5fe5ef1327ef08d3b78e8856
gyutae <qoxowkd0716@gmail.com>
2024-02-26 19:51:51 +0900

    feat: createPost 메서드 구현

---
+++ src/main/java/baegyutae/portfolio/controller/PostController.java
@@ -0,0 +1,25 @@
+package baegyutae.portfolio.controller;
+
+import baegyutae.portfolio.dto.PostCreateDto;
+import baegyutae.service.impl.PostServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/posts")
+@RequiredArgsConstructor
+public class PostController {
+
+    private final PostServiceImpl postServiceImpl;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Long createPost(@RequestBody PostCreateDto postCreateDto) {
+        return postServiceImpl.createPost(postCreateDto);
+    }
+}
