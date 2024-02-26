2ed09e623bc7943ec548a9d8f5fd11d28e754edb
gyutae <qoxowkd0716@gmail.com>
2024-02-26 20:11:55 +0900

    Feat: updatePost 구현

--- src/main/java/baegyutae/portfolio/service/PostService.java
+++ src/main/java/baegyutae/portfolio/service/PostService.java
@@ -2,6 +2,7 @@ package baegyutae.portfolio.service;
 
 import baegyutae.portfolio.dto.PostCreateDto;
 import baegyutae.portfolio.dto.PostResponseDto;
+import baegyutae.portfolio.dto.PostUpdateDto;
 import java.util.List;
 
 public interface PostService {
@@ -9,4 +10,5 @@ public interface PostService {
     Long createPost(PostCreateDto postCreateDto);
     List<PostResponseDto> getAllPosts();
     PostResponseDto getPostById(Long id);
+    void updatePost(Long id, PostUpdateDto postUpdateDto);
 }
