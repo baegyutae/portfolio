c0cde1278691fd0f5bb9da9c423181c3238a92ee
gyutae <qoxowkd0716@gmail.com>
2024-02-26 18:08:23 +0900

    feat: Post 엔티티 구현

---
+++ src/main/java/baegyutae/portfolio/domain/model/Post.java
@@ -0,0 +1,26 @@
+package baegyutae.portfolio.domain.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.time.LocalDateTime;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    @Column(length = 10000)
+    private String content;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
