5b850a3561571205bf51ebe5f7fb103f922d0cda
gyutae <qoxowkd0716@gmail.com>
2024-03-04 21:02:33 +0900

    Feat: UploadedFileService 구현체 구현

---
+++ src/main/java/baegyutae/portfolio/service/impl/UploadedFileServiceImpl.java
@@ -0,0 +1,25 @@
+package baegyutae.portfolio.service.impl;
+
+import baegyutae.portfolio.domain.model.UploadedFile;
+import baegyutae.portfolio.domain.repository.UploadedFileRepository;
+import baegyutae.portfolio.service.UploadedFileService;
+import java.time.LocalDateTime;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UploadedFileServiceImpl implements UploadedFileService {
+
+    private final UploadedFileRepository uploadedFileRepository;
+
+    @Override
+    public UploadedFile saveFileMetadata(String fileName, String fileUrl) {
+        UploadedFile uploadedFile = UploadedFile.builder()
+            .fileName(fileName)
+            .fileUrl(fileUrl)
+            .uploadTime(LocalDateTime.now())
+            .build();
+        return uploadedFileRepository.save(uploadedFile);
+    }
+}
