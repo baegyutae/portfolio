03344797f50b9e02eb1760d53812d8e8b8374489
gyutae <qoxowkd0716@gmail.com>
2024-03-05 20:51:31 +0900

    Feat: 움짤 업로드 로직 구현

--- src/main/java/baegyutae/portfolio/service/S3Service.java
+++ src/main/java/baegyutae/portfolio/service/S3Service.java
@@ -3,11 +3,15 @@ package baegyutae.portfolio.service;
 import com.amazonaws.HttpMethod;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import java.io.IOException;
 import java.net.URL;
 import java.util.Date;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 @RequiredArgsConstructor
@@ -24,5 +28,20 @@ public class S3Service {
                 .withExpiration(expiration);
 
         return amazonS3.generatePresignedUrl(generatePresignedUrlRequest);
+    }
+
+    public String uploadFile(MultipartFile file) {
+        String fileName = file.getOriginalFilename();
+        try {
+            ObjectMetadata objectMetadata = new ObjectMetadata();
+            objectMetadata.setContentType(file.getContentType());
+            objectMetadata.setContentLength(file.getSize());
+
+            amazonS3.putObject(
+                new PutObjectRequest(bucketName, fileName, file.getInputStream(), objectMetadata));
+            return amazonS3.getUrl(bucketName, fileName).toString();
+        } catch (IOException e) {
+            throw new RuntimeException("S3 파일 업로드 중 오류 발생", e);
+        }
     }
 }
