d16c08df0eba3cbed54904d68327f4c29b94265d
gyutae <qoxowkd0716@gmail.com>
2024-03-11 16:42:25 +0900

    Feat: User 엔티티 구현

---
+++ src/main/java/baegyutae/portfolio/entity/User.java
@@ -0,0 +1,42 @@
+package baegyutae.portfolio.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String name;
+
+    private LocalDateTime registeredAt = LocalDateTime.now();
+
+    @Builder
+    public User(String username, String password, String email, String name) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.name = name;
+    }
+}
