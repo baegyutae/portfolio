759e7335b5c2693b4e44fc8c01372f01b6501ff9
gyutae <qoxowkd0716@gmail.com>
2024-03-13 14:31:53 +0900

    Feat: 인증 조건 추가

--- src/main/java/baegyutae/portfolio/dto/SignupRequestDto.java
+++ src/main/java/baegyutae/portfolio/dto/SignupRequestDto.java
@@ -1,8 +1,18 @@
 package baegyutae.portfolio.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
 public record SignupRequestDto(
+    @Email(message = "올바른 이메일 형식이 아닙니다.")
     String email,
+
+    @NotBlank(message = "사용자 이름은 필수입니다.")
     String username,
+
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$",
+        message = "비밀번호는 숫자, 소문자, 대문자, 특수문자를 포함하여 8자 이상이어야 합니다.")
     String password
 ) {
 }
