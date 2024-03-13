143bddb930ec54b97da1ab7a903f907437e65e95
gyutae <qoxowkd0716@gmail.com>
2024-03-13 13:49:27 +0900

    Feat: 회원 가입 로직 추가

--- src/main/java/baegyutae/portfolio/service/impl/UserServiceImpl.java
+++ src/main/java/baegyutae/portfolio/service/impl/UserServiceImpl.java
@@ -1,7 +1,8 @@
 package baegyutae.portfolio.service.impl;
 
+import baegyutae.portfolio.dto.SignupRequestDto;
+import baegyutae.portfolio.dto.SignupResponseDto;
 import baegyutae.portfolio.dto.UserLoginDto;
-import baegyutae.portfolio.dto.UserRegistrationDto;
 import baegyutae.portfolio.dto.UserResponseDto;
 import baegyutae.portfolio.entity.User;
 import baegyutae.portfolio.repository.UserRepository;
@@ -18,22 +19,32 @@ public class UserServiceImpl implements UserService {
     private final PasswordEncoder passwordEncoder;
 
     @Override
-    public UserResponseDto registerUser(UserRegistrationDto registrationDto) {
-        User user = User.builder()
-            .username(registrationDto.username())
-            .password(passwordEncoder.encode(registrationDto.password()))
-            .build();
-        userRepository.save(user);
-        return new UserResponseDto(user.getId(), user.getUsername());
-    }
-
-    @Override
     public UserResponseDto loginUser(UserLoginDto loginDto) {
         User user = userRepository.findByUsername(loginDto.username());
-        if(user != null && passwordEncoder.matches(loginDto.password(), user.getPassword())) {
+        if (user != null && passwordEncoder.matches(loginDto.password(), user.getPassword())) {
             return new UserResponseDto(user.getId(), user.getUsername());
         } else {
             throw new RuntimeException("Login failed.");
         }
     }
+
+    @Override
+    public SignupResponseDto signupUser(SignupRequestDto signupRequestDto) {
+        if (userRepository.existsByUsername(signupRequestDto.username())
+            || userRepository.existsByEmail(signupRequestDto.email())) {
+            throw new IllegalStateException("Username or email already in use");
+        }
+
+        User newUser = User.builder()
+            .username(signupRequestDto.username())
+            .email(signupRequestDto.email())
+            .password(passwordEncoder.encode(signupRequestDto.password()))
+            .build();
+
+        User savedUser = userRepository.save(newUser);
+
+        return new SignupResponseDto(savedUser.getId(), savedUser.getUsername(),
+            savedUser.getEmail());
+    }
+
 }
