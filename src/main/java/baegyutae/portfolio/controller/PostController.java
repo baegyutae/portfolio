701fe81f3bff67d6193624fbc54d0eae0ee6e0e0
gyutae <qoxowkd0716@gmail.com>
2024-03-13 13:51:32 +0900

    Feat: 회원가입 API 추가

--- src/main/java/baegyutae/portfolio/controller/PostController.java
+++ src/main/java/baegyutae/portfolio/controller/PostController.java
@@ -11,7 +11,6 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,8 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
-@RequestMapping("/posts")
+@RequestMapping("/api/posts")
 @RequiredArgsConstructor
 public class PostController {
 
