dcb35d8e2b85337937e96f827dbca636f8577256
gyutae <qoxowkd0716@gmail.com>
2024-03-14 22:58:10 +0900

    Feat: token 로직 추가

--- src/main/java/baegyutae/portfolio/service/impl/UserServiceImpl.java
+++ src/main/java/baegyutae/portfolio/service/impl/UserServiceImpl.java
@@ -3,11 +3,14 @@ package baegyutae.portfolio.service.impl;
 import baegyutae.portfolio.dto.SignupRequestDto;
 import baegyutae.portfolio.dto.SignupResponseDto;
 import baegyutae.portfolio.dto.UserLoginDto;
-import baegyutae.portfolio.dto.UserResponseDto;
 import baegyutae.portfolio.entity.User;
 import baegyutae.portfolio.repository.UserRepository;
+import baegyutae.portfolio.security.JwtTokenUtil;
 import baegyutae.portfolio.service.UserService;
+import java.util.ArrayList;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -17,21 +20,31 @@ public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final JwtTokenUtil jwtTokenUtil;
 
     @Override
-    public UserResponseDto loginUser(UserLoginDto loginDto) {
-        User user = userRepository.findByUsername(loginDto.username());
-        if (user != null && passwordEncoder.matches(loginDto.password(), user.getPassword())) {
-            return new UserResponseDto(user.getId(), user.getUsername());
-        } else {
-            throw new RuntimeException("Login failed.");
-        }
+    public String loginUser(UserLoginDto loginDto) {
+        return userRepository.findByUsername(loginDto.username())
+            .map(user -> {
+                if (!passwordEncoder.matches(loginDto.password(), user.getPassword())) {
+                    throw new RuntimeException("비밀번호가 일치하지 않습니다.");
+                }
+                final UserDetails userDetails = loadUserByUsername(loginDto.username());
+                return jwtTokenUtil.generateToken(userDetails);
+            })
+            .orElseThrow(() -> new RuntimeException("사용자를 찾을 수 없습니다."));
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) {
+        return userRepository.findByUsername(username)
+            .map(user -> new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), new ArrayList<>()))
+            .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
     }
 
     @Override
     public SignupResponseDto signupUser(SignupRequestDto signupRequestDto) {
-        if (userRepository.existsByUsername(signupRequestDto.username())
-            || userRepository.existsByEmail(signupRequestDto.email())) {
+        if (userRepository.existsByUsername(signupRequestDto.username()) || userRepository.existsByEmail(signupRequestDto.email())) {
             throw new IllegalStateException("Username or email already in use");
         }
 
@@ -43,8 +56,6 @@ public class UserServiceImpl implements UserService {
 
         User savedUser = userRepository.save(newUser);
 
-        return new SignupResponseDto(savedUser.getId(), savedUser.getUsername(),
-            savedUser.getEmail());
+        return new SignupResponseDto(savedUser.getId(), savedUser.getUsername(), savedUser.getEmail());
     }
-
 }
