7fb895078751755cf29aec44c9c3c5e74b35528b
gyutae <qoxowkd0716@gmail.com>
2024-03-14 22:58:38 +0900

    Feat: loadUserByUsername 메서드 추가

--- src/main/java/baegyutae/portfolio/service/UserService.java
+++ src/main/java/baegyutae/portfolio/service/UserService.java
@@ -3,11 +3,15 @@ package baegyutae.portfolio.service;
 import baegyutae.portfolio.dto.SignupRequestDto;
 import baegyutae.portfolio.dto.SignupResponseDto;
 import baegyutae.portfolio.dto.UserLoginDto;
-import baegyutae.portfolio.dto.UserResponseDto;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 public interface UserService {
 
+    // UserDetails 반환
+    UserDetails loadUserByUsername(String username) throws UsernameNotFoundException;
+
     SignupResponseDto signupUser(SignupRequestDto signupRequestDto);
 
-    UserResponseDto loginUser(UserLoginDto loginDto);
+    String loginUser(UserLoginDto loginDto) throws Exception;
 }
