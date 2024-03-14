c609319db59cd88d46e53e23697d53c4d804937f
gyutae <qoxowkd0716@gmail.com>
2024-03-14 23:02:48 +0900

    Feat: Optional 구현

--- src/main/java/baegyutae/portfolio/repository/UserRepository.java
+++ src/main/java/baegyutae/portfolio/repository/UserRepository.java
@@ -1,10 +1,11 @@
 package baegyutae.portfolio.repository;
 
 import baegyutae.portfolio.entity.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
+    Optional<User> findByUsername(String username);
     boolean existsByEmail(String email);
     boolean existsByUsername(String username);
 }
