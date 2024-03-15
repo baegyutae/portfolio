abebce71359a71e32bc5e76f3082308cf74277c6
gyutae <qoxowkd0716@gmail.com>
2024-03-15 13:34:27 +0900

    Feat: 댓글 조회 기능 구현

--- src/main/java/baegyutae/portfolio/service/CommentService.java
+++ src/main/java/baegyutae/portfolio/service/CommentService.java
@@ -2,7 +2,9 @@ package baegyutae.portfolio.service;
 
 import baegyutae.portfolio.dto.CommentCreateRequestDto;
 import baegyutae.portfolio.dto.CommentResponseDto;
+import java.util.List;
 
 public interface CommentService {
     CommentResponseDto createComment(Long postId, Long userId, CommentCreateRequestDto requestDto);
+    List<CommentResponseDto> findAllCommentsByPostId(Long postId);
 }
