411e3e5ee41f2089748157fb7e58fd42b858903f
gyutae <qoxowkd0716@gmail.com>
2024-03-15 13:34:32 +0900

    Feat: 댓글 조회 기능 구현

--- src/main/java/baegyutae/portfolio/controller/CommentController.java
+++ src/main/java/baegyutae/portfolio/controller/CommentController.java
@@ -3,9 +3,11 @@ package baegyutae.portfolio.controller;
 import baegyutae.portfolio.dto.CommentCreateRequestDto;
 import baegyutae.portfolio.dto.CommentResponseDto;
 import baegyutae.portfolio.service.CommentService;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -28,5 +30,11 @@ public class CommentController {
         CommentResponseDto createdComment = commentService.createComment(postId, userId,
             requestDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(createdComment);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CommentResponseDto>> getAllCommentsByPostId(@PathVariable Long postId) {
+        List<CommentResponseDto> comments = commentService.findAllCommentsByPostId(postId);
+        return ResponseEntity.ok(comments);
     }
 }
