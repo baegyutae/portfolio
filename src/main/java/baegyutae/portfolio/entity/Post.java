b0b415d4c0fa990bea7d8db9dd47742a59dda419
gyutae <qoxowkd0716@gmail.com>
2024-03-15 13:12:47 +0900

    Feat: 댓글 관계 설정

--- src/main/java/baegyutae/portfolio/entity/Post.java
+++ src/main/java/baegyutae/portfolio/entity/Post.java
@@ -1,11 +1,16 @@
 package baegyutae.portfolio.entity;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -29,6 +34,9 @@ public class Post {
     private LocalDateTime updatedAt;
 
     private String imageUrl;
+
+    @OneToMany(mappedBy = "post", fetch = FetchType.LAZY)
+    private List<Comment> comments = new ArrayList<>();
 
     @Builder
     public Post(String title, String content, String imageUrl) {
