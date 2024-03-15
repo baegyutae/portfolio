58c03223bd6efa6ec2f90c928ff747a4de90037d
gyutae <qoxowkd0716@gmail.com>
2024-03-15 13:15:52 +0900

    Feat: 댓글 생성 기능 구현

---
+++ src/main/java/baegyutae/portfolio/controller/CommentController.java
@@ -0,0 +1,32 @@
+package baegyutae.portfolio.controller;
+
+import baegyutae.portfolio.dto.CommentCreateRequestDto;
+import baegyutae.portfolio.dto.CommentResponseDto;
+import baegyutae.portfolio.service.CommentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/posts/{postId}/comments")
+@RequiredArgsConstructor
+public class CommentController {
+
+    private final CommentService commentService;
+
+    @PostMapping
+    public ResponseEntity<CommentResponseDto> createComment(
+        @PathVariable Long postId,
+        @RequestParam("userId") Long userId,
+        @RequestBody CommentCreateRequestDto requestDto) {
+        CommentResponseDto createdComment = commentService.createComment(postId, userId,
+            requestDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdComment);
+    }
+}
