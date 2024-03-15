4987fe9cd8f960edc25ba1dd4eb3ec83eb49de34
gyutae <qoxowkd0716@gmail.com>
2024-03-15 13:48:46 +0900

    Feat: 댓글 수정 기능 구현

--- src/main/java/baegyutae/portfolio/service/CommentService.java
+++ src/main/java/baegyutae/portfolio/service/CommentService.java
@@ -2,9 +2,11 @@ package baegyutae.portfolio.service;
 
 import baegyutae.portfolio.dto.CommentCreateRequestDto;
 import baegyutae.portfolio.dto.CommentResponseDto;
+import baegyutae.portfolio.dto.CommentUpdateRequestDto;
 import java.util.List;
 
 public interface CommentService {
     CommentResponseDto createComment(Long postId, Long userId, CommentCreateRequestDto requestDto);
     List<CommentResponseDto> findAllCommentsByPostId(Long postId);
+    CommentResponseDto updateComment(Long commentId, CommentUpdateRequestDto requestDto);
 }
