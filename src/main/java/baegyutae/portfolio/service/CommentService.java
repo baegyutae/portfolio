de23b1056928db7dee6ec38ee09a15774b85f29f
gyutae <qoxowkd0716@gmail.com>
2024-03-28 23:49:22 +0900

    Feat: 페이지 네이션 메서드 추가

--- src/main/java/baegyutae/portfolio/service/CommentService.java
+++ src/main/java/baegyutae/portfolio/service/CommentService.java
@@ -4,10 +4,13 @@ import baegyutae.portfolio.dto.CommentCreateRequestDto;
 import baegyutae.portfolio.dto.CommentResponseDto;
 import baegyutae.portfolio.dto.CommentUpdateRequestDto;
 import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface CommentService {
     CommentResponseDto createComment(Long postId, Long userId, CommentCreateRequestDto requestDto);
     List<CommentResponseDto> findAllCommentsByPostId(Long postId);
     CommentResponseDto updateComment(Long commentId, CommentUpdateRequestDto requestDto);
     void deleteComment(Long commentId);
+    Page<CommentResponseDto> findAllCommentsByPostId(Long postId, Pageable pageable);
 }
