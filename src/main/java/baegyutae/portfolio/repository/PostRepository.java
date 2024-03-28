a5906b7af7bcc74712b608369c3f6f9dc5974b91
gyutae <qoxowkd0716@gmail.com>
2024-03-29 03:52:11 +0900

    Remote: JOIN FETCH 쿼리 삭제

--- src/main/java/baegyutae/portfolio/repository/PostRepository.java
+++ src/main/java/baegyutae/portfolio/repository/PostRepository.java
@@ -1,13 +1,8 @@
 package baegyutae.portfolio.repository;
 
 import baegyutae.portfolio.entity.Post;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
-    @Query("SELECT p FROM Post p LEFT JOIN FETCH p.comments c LEFT JOIN FETCH c.user WHERE p.id = :postId")
-    Optional<Post> findByIdWithCommentsAndUser(@Param("postId") Long postId);
 }
