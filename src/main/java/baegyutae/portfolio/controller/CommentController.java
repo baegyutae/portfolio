6b0a97634cfbf4678b4f253bdf5892ca9595caac
gyutae <qoxowkd0716@gmail.com>
2024-04-06 01:55:17 +0900

    Feat: Api 통일 로직 추가

--- src/main/java/baegyutae/portfolio/controller/CommentController.java
+++ src/main/java/baegyutae/portfolio/controller/CommentController.java
@@ -1,11 +1,10 @@
 package baegyutae.portfolio.controller;
 
-import static baegyutae.portfolio.util.SecurityUtils.getCurrentUserId;
-
 import baegyutae.portfolio.dto.CommentCreateRequestDto;
 import baegyutae.portfolio.dto.CommentResponseDto;
 import baegyutae.portfolio.dto.CommentUpdateRequestDto;
-import baegyutae.portfolio.exception.UserNotAuthenticatedException;
+import baegyutae.portfolio.response.ApiResponse;
+import baegyutae.portfolio.security.UserDetailsImpl;
 import baegyutae.portfolio.service.CommentService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -14,6 +13,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -31,42 +31,45 @@ public class CommentController {
     private final CommentService commentService;
 
     @PostMapping
-    public ResponseEntity<CommentResponseDto> createComment(
+    public ResponseEntity<ApiResponse<CommentResponseDto>> createComment(
         @PathVariable Long postId,
-        @Valid @RequestBody CommentCreateRequestDto requestDto
+        @Valid @RequestBody CommentCreateRequestDto requestDto,
+        @AuthenticationPrincipal UserDetailsImpl userDetails
     ) {
-        Long userId = getCurrentUserId();
-        if (userId == null) {
-            throw new UserNotAuthenticatedException("인증된 사용자가 아닙니다.");
-        }
-
+        Long userId = userDetails.getUser().getId();
         CommentResponseDto responseDto = commentService.createComment(postId, userId, requestDto);
-        return ResponseEntity.status(HttpStatus.CREATED).body(responseDto);
+        return new ResponseEntity<>(ApiResponse.success(responseDto), HttpStatus.CREATED);
     }
 
     @GetMapping
-    public ResponseEntity<Page<CommentResponseDto>> getAllCommentsByPostId(
-        @PathVariable Long postId, @PageableDefault(size = 5) Pageable pageable) {
+    public ResponseEntity<ApiResponse<Page<CommentResponseDto>>> getAllCommentsByPostId(
+        @PathVariable Long postId, @PageableDefault(size = 5) Pageable pageable
+    ) {
         Page<CommentResponseDto> comments = commentService.findAllCommentsByPostId(postId,
             pageable);
-        return ResponseEntity.ok(comments);
+        return ResponseEntity.ok(ApiResponse.success(comments));
     }
 
     @PatchMapping("/{commentId}")
-    public ResponseEntity<CommentResponseDto> updateComment(
+    public ResponseEntity<ApiResponse<CommentResponseDto>> updateComment(
         @PathVariable Long postId,
         @PathVariable Long commentId,
-        @RequestBody CommentUpdateRequestDto requestDto) {
-        Long userId = getCurrentUserId();
-        CommentResponseDto updatedComment = commentService.updateComment(commentId, requestDto, userId);
-        return ResponseEntity.ok(updatedComment);
+        @RequestBody CommentUpdateRequestDto requestDto,
+        @AuthenticationPrincipal UserDetailsImpl userDetails
+    ) {
+        Long userId = userDetails.getUser().getId();
+        CommentResponseDto updatedComment = commentService.updateComment(commentId, requestDto,
+            userId);
+        return ResponseEntity.ok(ApiResponse.success(updatedComment));
     }
 
     @DeleteMapping("/{commentId}")
-    public ResponseEntity<Void> deleteComment(@PathVariable Long postId,
-        @PathVariable Long commentId) {
-        Long userId = getCurrentUserId();
+    public ResponseEntity<ApiResponse<Void>> deleteComment(
+        @PathVariable Long postId, @PathVariable Long commentId,
+        @AuthenticationPrincipal UserDetailsImpl userDetails
+    ) {
+        Long userId = userDetails.getUser().getId();
         commentService.deleteComment(commentId, userId);
-        return ResponseEntity.noContent().build();
+        return ResponseEntity.ok(ApiResponse.success());
     }
 }
