7867a2fbdf28e8022613727b66bf316c075f8cf0
gyutae <qoxowkd0716@gmail.com>
2024-04-06 03:23:34 +0900

    Feat: 에러 로직 추가

--- src/main/java/baegyutae/portfolio/response/ApiResponse.java
+++ src/main/java/baegyutae/portfolio/response/ApiResponse.java
@@ -11,12 +11,17 @@ public class ApiResponse<T> {
 
     private final boolean success;
     private final T data;
+    private final ApiError error;
 
     public static <T> ApiResponse<T> success(T data) {
-        return new ApiResponse<>(true, data);
+        return new ApiResponse<>(true, data, null);
     }
 
     public static ApiResponse<Void> success() {
-        return new ApiResponse<>(true, null);
+        return new ApiResponse<>(true, null, null);
+    }
+
+    public static <T> ApiResponse<T> error(ApiError error) {
+        return new ApiResponse<>(false, null, error);
     }
 }
