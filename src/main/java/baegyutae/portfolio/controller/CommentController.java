be60c5807558d601ceb7185b706194dad7e49ddd
gyutae <qoxowkd0716@gmail.com>
2024-04-05 23:43:44 +0900

    Feat: 사용자 인증 로직 구현

--- src/main/java/baegyutae/portfolio/controller/CommentController.java
+++ src/main/java/baegyutae/portfolio/controller/CommentController.java
@@ -5,6 +5,7 @@ import static baegyutae.portfolio.util.SecurityUtils.getCurrentUserId;
 import baegyutae.portfolio.dto.CommentCreateRequestDto;
 import baegyutae.portfolio.dto.CommentResponseDto;
 import baegyutae.portfolio.dto.CommentUpdateRequestDto;
+import baegyutae.portfolio.exception.UserNotAuthenticatedException;
 import baegyutae.portfolio.service.CommentService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -30,11 +31,14 @@ public class CommentController {
     private final CommentService commentService;
 
     @PostMapping
-    public ResponseEntity<?> createComment(
+    public ResponseEntity<CommentResponseDto> createComment(
         @PathVariable Long postId,
         @Valid @RequestBody CommentCreateRequestDto requestDto
     ) {
         Long userId = getCurrentUserId();
+        if (userId == null) {
+            throw new UserNotAuthenticatedException("인증된 사용자가 아닙니다.");
+        }
 
         CommentResponseDto responseDto = commentService.createComment(postId, userId, requestDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(responseDto);
@@ -53,14 +57,16 @@ public class CommentController {
         @PathVariable Long postId,
         @PathVariable Long commentId,
         @RequestBody CommentUpdateRequestDto requestDto) {
-        CommentResponseDto updatedComment = commentService.updateComment(commentId, requestDto);
+        Long userId = getCurrentUserId();
+        CommentResponseDto updatedComment = commentService.updateComment(commentId, requestDto, userId);
         return ResponseEntity.ok(updatedComment);
     }
 
     @DeleteMapping("/{commentId}")
     public ResponseEntity<Void> deleteComment(@PathVariable Long postId,
         @PathVariable Long commentId) {
-        commentService.deleteComment(commentId);
+        Long userId = getCurrentUserId();
+        commentService.deleteComment(commentId, userId);
         return ResponseEntity.noContent().build();
     }
 }
