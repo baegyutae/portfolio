4e8a05846bb639cbdaeeb46f735887c76ab6ec2d
gyutae <qoxowkd0716@gmail.com>
2024-04-06 01:54:46 +0900

    Feat: 예외 처리 메서드 추가

--- src/main/java/baegyutae/portfolio/exception/GlobalExceptionHandler.java
+++ src/main/java/baegyutae/portfolio/exception/GlobalExceptionHandler.java
@@ -1,5 +1,8 @@
 package baegyutae.portfolio.exception;
 
+import baegyutae.portfolio.response.ApiError;
+import baegyutae.portfolio.response.ApiResponse;
+import java.util.Collections;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -9,9 +12,31 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(UserNotAuthenticatedException.class)
-    public ResponseEntity<String> handleUserNotAuthenticatedException(
-        UserNotAuthenticatedException ex) {
-        // 사용자 인증 실패 예외에 대한 처리
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.UNAUTHORIZED);
+    public ResponseEntity<ApiResponse<ApiError>> handleUserNotAuthenticated(UserNotAuthenticatedException ex) {
+        ApiError apiError = ApiError.builder()
+            .message("인증되지 않은 사용자입니다.")
+            .errors(Collections.singletonList(ex.getMessage()))
+            .build();
+
+        ApiResponse<ApiError> response = ApiResponse.<ApiError>builder()
+            .data(apiError)
+            .build();
+
+        return new ResponseEntity<>(response, HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponse<ApiError>> handleAllExceptions(
+        Exception ex) {
+
+        ApiError apiError = ApiError.builder()
+            .message(ex.getMessage())
+            .errors(Collections.singletonList("오류가 발생했습니다."))
+            .build();
+
+        ApiResponse<ApiError> response = ApiResponse.<ApiError>builder()
+            .data(apiError)
+            .build();
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
