826de5fe11babc057a52500ba36e064597b55def
gyutae <qoxowkd0716@gmail.com>
2024-04-06 03:23:19 +0900

    Feat: 예외 처리 기능 추가

--- src/main/java/baegyutae/portfolio/controller/FileUploadController.java
+++ src/main/java/baegyutae/portfolio/controller/FileUploadController.java
@@ -1,12 +1,17 @@
 package baegyutae.portfolio.controller;
 
-import baegyutae.portfolio.entity.UploadedFile;
 import baegyutae.portfolio.dto.FileUploadRequest;
+import baegyutae.portfolio.entity.UploadedFile;
+import baegyutae.portfolio.response.ApiError;
+import baegyutae.portfolio.response.ApiResponse;
 import baegyutae.portfolio.service.S3Service;
 import baegyutae.portfolio.service.UploadedFileService;
+import com.amazonaws.services.s3.model.AmazonS3Exception;
 import java.net.URL;
 import java.util.Date;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,13 +27,28 @@ public class FileUploadController {
     private final UploadedFileService uploadedFileService;
 
     @PostMapping("/upload")
-    public ResponseEntity<UploadedFile> uploadFile(@RequestBody FileUploadRequest request) {
-        // 파일을 업로드할 S3의 프리사인드 URL을 생성합니다.
-        URL presignedUrl = s3Service.generatePresignedUrl(request.fileName(), new Date(System.currentTimeMillis() + 1000 * 60 * 60)); // 예: 1시간 후 만료
+    public ResponseEntity<ApiResponse<UploadedFile>> uploadFile(
+        @RequestBody FileUploadRequest request) {
+        try {
+            URL presignedUrl = s3Service.generatePresignedUrl(request.fileName(),
+                new Date(System.currentTimeMillis() + 1000 * 60 * 60));
+            UploadedFile uploadedFile = uploadedFileService.saveFileMetadata(request.fileName(),
+                presignedUrl.toString());
 
-        // 업로드된 파일의 메타데이터를 저장합니다.
-        UploadedFile uploadedFile = uploadedFileService.saveFileMetadata(request.fileName(), presignedUrl.toString());
-
-        return ResponseEntity.ok(uploadedFile);
+            return ResponseEntity.ok(ApiResponse.success(uploadedFile));
+        } catch (AmazonS3Exception e) {
+            ApiError apiError = ApiError.builder()
+                .message("S3 서비스 엑세스 오류")
+                .errors(List.of(e.getMessage()))
+                .build();
+            return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE)
+                .body(ApiResponse.error(apiError));
+        } catch (Exception e) {
+            ApiError apiError = ApiError.builder()
+                .message("파일 업로드 중 예상치 못한 오류가 발생했습니다.")
+                .errors(List.of(e.getMessage()))
+                .build();
+            return ResponseEntity.internalServerError().body(ApiResponse.error(apiError));
+        }
     }
 }
