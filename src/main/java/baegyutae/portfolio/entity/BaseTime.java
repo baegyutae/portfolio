34add67b0ae36080b8d7496fc171a29cd28c9008
gyutae <qoxowkd0716@gmail.com>
2024-04-07 02:20:01 +0900

    Refactor: BaseTime 분리

---
+++ src/main/java/baegyutae/portfolio/entity/BaseTime.java
@@ -0,0 +1,21 @@
+package baegyutae.portfolio.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Getter
+@MappedSuperclass
+public abstract class BaseTime {
+
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+}
