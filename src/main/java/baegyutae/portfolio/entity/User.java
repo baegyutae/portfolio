86f9d1bbaebf04356432bf6a53405e560385983c
gyutae <qoxowkd0716@gmail.com>
2024-04-07 03:14:00 +0900

    Feat: Validation 적용 및 오버라이딩 적용

--- src/main/java/baegyutae/portfolio/entity/User.java
+++ src/main/java/baegyutae/portfolio/entity/User.java
@@ -1,10 +1,12 @@
 package baegyutae.portfolio.entity;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -19,12 +21,16 @@ public class User extends BaseTime {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "사용자 이름은 필수입니다.")
     private String username;
 
-    @Column(nullable = false)
+    @NotBlank(message = "비밀번호는 필수입니다.")
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$",
+        message = "비밀번호는 숫자, 소문자, 대문자, 특수문자를 포함하여 8자 이상이어야 합니다.")
     private String password;
 
-    @Column(nullable = false, unique = true)
+    @Email(message = "유효한 이메일 주소를 입력해주세요.")
+    @NotBlank(message = "이메일은 필수입니다.")
     private String email;
 
     @Builder
@@ -32,5 +38,22 @@ public class User extends BaseTime {
         this.username = username;
         this.password = password;
         this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return email != null ? email.equals(user.email) : user.email == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return email != null ? email.hashCode() : 0;
     }
 }
