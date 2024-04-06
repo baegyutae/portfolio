6471aa272af8b028f311136abd361d4d2d5423dc
gyutae <qoxowkd0716@gmail.com>
2024-04-07 03:43:21 +0900

    Feat: Validation 적용

--- src/main/java/baegyutae/portfolio/entity/UploadedFile.java
+++ src/main/java/baegyutae/portfolio/entity/UploadedFile.java
@@ -4,10 +4,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.URL;
 
 @Entity
 @Getter
@@ -18,7 +21,12 @@ public class UploadedFile extends BaseTime {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "파일 이름은 필수입니다.")
+    @Size(max = 255, message = "파일 이름은 255자를 초과할 수 없습니다.")
     private String fileName;
+
+    @NotBlank(message = "파일 URL은 필수입니다.")
+    @URL(message = "유효한 URL 형식이어야 합니다.")
     private String fileUrl;
 
     @Builder
