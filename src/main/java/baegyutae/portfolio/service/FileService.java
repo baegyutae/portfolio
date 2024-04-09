ac30e8d4c6194455593dadc2b6da302fb76d0334
gyutae <qoxowkd0716@gmail.com>
2024-04-09 23:27:17 +0900

    Remove: 안쓰는 클래스 삭제

--- src/main/java/baegyutae/portfolio/service/FileService.java
+++
@@ -1,9 +0,0 @@
-package baegyutae.portfolio.service;
-
-import java.net.URL;
-import java.util.Date;
-
-public interface FileService {
-
-    URL generatePresignedUrl(String bucketName, String objectKey, Date expiration);
-}
