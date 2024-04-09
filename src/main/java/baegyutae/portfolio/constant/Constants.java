33c38f54d692fff0f7103560de62b4d7d49c3f89
gyutae <qoxowkd0716@gmail.com>
2024-04-09 23:27:38 +0900

    Feat: 상수 클래스 생성

---
+++ src/main/java/baegyutae/portfolio/constant/Constants.java
@@ -0,0 +1,8 @@
+package baegyutae.portfolio.constant;
+
+public class Constants {
+    public static final String PASSWORD_MISMATCH_ERROR = "비밀번호가 일치하지 않습니다.";
+    public static final String USER_NOT_FOUND_ERROR = "사용자를 찾을 수 없습니다.";
+    public static final String USER_NOT_FOUND_WITH_USERNAME = "사용자를 찾을 수 없습니다.";
+    public static final String USERNAME_OR_EMAIL_ALREADY_IN_USE = "사용자 이름 또는 이메일이 이미 사용 중입니다.";
+}
